67d8ee5ab4b1c4c9746dea54ca03d95aec00c574
liusiyuan <liusiyuan@amax3.onmypc.net>
2016-11-26 00:32:43 +0800

    add baseline

---
+++ baseline/empty.sh
@@ -0,0 +1,8 @@
+touch train/1_F/.gitignore
+touch train/1_EN/.gitignore
+touch train/1_NM/.gitignore
+touch dataset/train/.gitignore
+touch model/1_F/.gitignore
+touch model/1_EN/.gitignore
+touch model/1_NM/.gitignore
+touch log/.gitignore
